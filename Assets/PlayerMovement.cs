b548d25d9f7b48194c8f41364e3e188a6bb70410
Syed Thoufiq <syedth.thoufiq@gmail.com>
2018-03-18 21:41:05 +0530

    Initial Commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public Rigidbody rb;
+	
+	// Use Fixed Update when Using Physics
+	void FixedUpdate () {
+		rb.AddForce(0 , 0, 2000 * Time.deltaTime);
+
+	}
+}
